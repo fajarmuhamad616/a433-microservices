4093800edf2148031eac4e69dae1b6a6e1a3bc63
root <fajarmuhamad616@gmail.com>
2025-01-23 00:50:55 -0500

    Add CI and bash

---
+++ build_push_image_karsajobs-ui.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Variable untuk namespace/username, image, dan tag
+IMAGE_NAME_APP="karsajobs-ui"
+TAG_APP="latest"
+GITHUB_USERNAME="fajarmuhamad616"
+
+# 1. Build Docker Image dengan tag latest
+docker build -t $IMAGE_NAME_APP:$TAG_APP .
+
+# 2. Ubah nama image sesuai format Github packages
+# Format: <namespace/username>/<repository>:<tag_version>
+docker tag $IMAGE_NAME_APP:$TAG_APP ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME_APP:$TAG_APP 
+
+# 3. Login ke Github packages
+docker login ghcr.io -u $GITHUB_USERNAME -p $GHCR_TOKEN
+
+# 4. Push image ke Docker Hub
+docker push ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME_APP:$TAG_APP
